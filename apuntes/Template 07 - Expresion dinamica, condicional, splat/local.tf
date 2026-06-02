cafe8b48b7a5d078f63c19698839b0600f498a97
fabricioAaron <k.fabriciocastilloleon@gmail.com>
2026-06-02 21:23:23 +0200

    test1

---
+++ apuntes/Template 07 - Expresion dinamica, condicional, splat/local.tf
@@ -0,0 +1,16 @@
+locals {
+  nsg_rules = { #aquí se define un mapa de reglas de seguridad
+    "allow_http" = {
+      priority               = 100
+      destination_port_range = "80"
+      description            = "Allow HTTP"
+
+    }
+    https = {
+      priority               = 110
+      destination_port_range = "443"
+      description            = "Allow HTTPS"
+
+    }
+  }
+}
