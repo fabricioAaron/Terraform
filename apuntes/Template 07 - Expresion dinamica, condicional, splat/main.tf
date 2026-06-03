2a4300b4c0e32c6599a65c23770c77aa7701c0cd
fabricioAaron <k.fabriciocastilloleon@gmail.com>
2026-06-03 14:22:05 +0200

    apuntes template

--- apuntes/Template 07 - Expresion dinamica, condicional, splat/main.tf
+++ apuntes/Template 07 - Expresion dinamica, condicional, splat/main.tf
@@ -1,25 +1,26 @@
-resource "azurerm_resource_group" "example" {
+resource "azurerm_resource_group" "rg" {
   name     = "example-resources"
   location = "West Europe"
 }
 
-resource "azurerm_network_security_group" "example" {
+resource "azurerm_network_security_group" "rg" {
   name                = "acceptanceTestSecurityGroup1"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
-  dynamic "security_rule" { #aquí se define un bloque dinámico que se va a ejecutar para cada elemento del mapa de reglas de seguridad siempre que va un dynamic se tiene que poner un content para definir el bloque que se va a ejecutarº
-    for_each = local.nsg_rules
+  dynamic "security_rule" {    #aquí se define un bloque dinámico que se va a ejecutar para cada elemento del mapa de reglas de seguridad siempre que va un dynamic se tiene que poner un content para definir el bloque que se va a ejecutarº
+    for_each = local.nsg_rules #esto sirve para iterar cada uno de los elementos que tenemos en el local
     content {
-      name                       = security_rule.value.key                    #aqui se define el nombre de la regla
+      name                       = security_rule.key                          #aqui se define el nombre de la regla dentro de local sería nuestro allow-http, allow-https
       priority                   = security_rule.value.priority               #aqui se define la prioridad de la regla
       direction                  = "Inbound"                                  #aqui se define la dirección de la regla
       access                     = "Allow"                                    #aqui se define el acceso de la regla
       protocol                   = "Tcp"                                      #aqui se define el protocolo de la regla
       source_port_range          = "*"                                        #aqui se define el puerto de origen de la regla
       destination_port_range     = security_rule.value.destination_port_range #aqui se define el puerto de destino de la regla
-      source_address_prefix      = security_rule.value.source_address_prefix
-      destination_address_prefix = security_rule.value.destination_address_prefix
+      source_address_prefix      = "*"
+      destination_address_prefix = "*"
+      description                = security_rule.value.description
     }
   }
 
--- apuntes/Template 07 - Expresion dinamica, condicional, splat/variables.tf
+++ apuntes/Template 07 - Expresion dinamica, condicional, splat/variables.tf
@@ -1,30 +1,30 @@
 variable "environment" { #aquí se define una variable llamada "environment", que es de tipo string, y tiene una descripción y un valor por defecto, en este caso el valor por defecto es "staging"
-  type = string
+  type        = string
   description = "el tipo de entorno"
-  default = "staging"
+  default     = "staging"
 }
 
 variable "storage_disk" { #aquí se define una variable llamada "storage_disk", que es de tipo number, y tiene una descripción y un valor por defecto, en este caso el valor por defecto es 40
-  type = number
+  type        = number
   description = "se añade el espacio del disco de nuesto mv (SO) "
-  default = 40
+  default     = 40
 }
 
 variable "vm_delete" {
-  type = bool
-  description =  "si se quiere eliminar el disco del SO al eliminar la máquina virtual"
-  default = true
+  type        = bool
+  description = "si se quiere eliminar el disco del SO al eliminar la máquina virtual"
+  default     = true
 }
 
 #list es una estructura de datos ordenada que puede contener elementos duplicados, y se accede a los elementos por índice, utilizando la función element() o la sintaxis de corchetes []
 variable "ubicacion_permitida" {
-  type = list(string)
+  type        = list(string)
   description = "lista de ubicaciones permitidas para desplegar los recursos"
-  default = ["Norway East", "Spain Central", "france central"]  
+  default     = ["Norway East", "Spain Central", "france central"]
 }
 
 variable "etiqueta_recursos" {
-  type = map(string) #aquí se define una variable llamada "etiqueta_recursos", que es de tipo mapa de string, y tiene una descripción y un valor por defecto, en este caso el valor por defecto es un mapa con una clave "environment" y un valor "staging"
+  type        = map(string) #aquí se define una variable llamada "etiqueta_recursos", que es de tipo mapa de string, y tiene una descripción y un valor por defecto, en este caso el valor por defecto es un mapa con una clave "environment" y un valor "staging"
   description = "etiquetas para aplicar a los recursos"
   default = {
     environment = "staging"
@@ -36,37 +36,37 @@ variable "etiqueta_recursos" {
 
 #tuple es una estructura de datos ordenada e inmutable que puede contener elementos de diferentes tipos, y se accede a los elementos por índice, utilizando la función element() o la sintaxis de corchetes []
 variable "configuración_network" {
-  type = tuple([ string, string, number ])
+  type        = tuple([string, string, number])
   description = "configuración de red para la máquina virtual( vnet adress, subnet address, y número de IPs privadas)"
-  default = [ "10.0.0.0/16", "10.0.2.0/24", 24 ]
+  default     = ["10.0.0.0/16", "10.0.2.0/24", 24]
 }
 
 
 variable "sizes_vm" {
-  type = list(string) #aquí se define una variable llamada "sizes_vm", que es de tipo lista de string, y tiene una descripción y un valor por defecto, en este caso el valor por defecto es una lista con tres tamaños de máquina virtual permitidos
+  type        = list(string) #aquí se define una variable llamada "sizes_vm", que es de tipo lista de string, y tiene una descripción y un valor por defecto, en este caso el valor por defecto es una lista con tres tamaños de máquina virtual permitidos
   description = "lista de tamaños de máquina virtual permitidos"
-  default = [ "Standard_B2as_v2", "Standard_B1as_v2", "Standard_B1s" ]
+  default     = ["Standard_B2as_v2", "Standard_B1as_v2", "Standard_B1s"]
 }
 
 # tipo objedo 
 
 variable "vm_config" {
   type = object({
-    size = string
+    size      = string
     publisher = string
-    offer = string
-    sku = string
-    version = string
+    offer     = string
+    sku       = string
+    version   = string
 
   })
   description = "configuración de la máquina virtual"
   default = {
-    size = "Standard_B2as_v2"
+    size      = "Standard_B2as_v2"
     publisher = "Canonical"
-    offer = "0001-com-ubuntu-server-jammy"
-    sku = "22_04-lts"
-    version = "latest"
-  }  
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
 }
 
 #variable "nombre_storage" {
@@ -75,6 +75,6 @@ variable "vm_config" {
 #}
 
 variable "for_each_nombre_storage" {
-  type = set(string) 
-  default = ["templatestorage12" ,"templatestorage13"]
+  type    = set(string)
+  default = ["templatestorage12", "templatestorage13"]
 }
